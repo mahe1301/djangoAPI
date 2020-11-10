6b5afabfe03e66bbc444f37953bf412111b75b53
mahe <mahe78611@gmail.com>
2020-11-10 06:31:09 +0530

    account api

--- restapiapp/views/useraccount.py
+++ restapiapp/views/useraccount.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import  status
 from rest_framework.decorators import api_view ,permission_classes
-from rest_framework.permissions import AllowAny #, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from ..models import CustomAccount
 
 
@@ -19,11 +19,18 @@ def create_user(request):
                 },
                 "status": "fail"
             }
-        if 'customer_xid' in request.data.keys():
-            user_obj=CustomAccount.objects.get(username=request.data['customer_xid'])
+        if 'customer_xid' in request.data.keys() and len(request.data['customer_xid']) > 0:
+            user_obj=CustomAccount.objects.filter(username=request.data['customer_xid'])
+            if len(user_obj) == 0:
+                user_obj=CustomAccount.objects.create_user(username=request.data['customer_xid'],password=request.data['customer_xid'])
+            else:
+                user_obj=user_obj[0]
             resp["data"] = user_obj.tokens()
             resp["status"] = "success"
+            resp_status=status.HTTP_201_CREATED
+        else:
+            raise Exception
     except Exception as e:
-        pass
+        resp_status=status.HTTP_400_BAD_REQUEST
     finally:
-        return Response(resp, status=status.HTTP_200_OK)
+        return Response(resp, status=resp_status)
